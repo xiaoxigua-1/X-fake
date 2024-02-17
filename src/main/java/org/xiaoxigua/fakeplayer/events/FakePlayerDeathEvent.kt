8eef471b8a2970fe225371ba0dd5c3ac7faafe84
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2024-02-17 19:24:56 +0800

    remove unused import

--- src/main/java/org/xiaoxigua/fakeplayer/events/FakePlayerDeathEvent.kt
+++ src/main/java/org/xiaoxigua/fakeplayer/events/FakePlayerDeathEvent.kt
@@ -1,11 +1,8 @@
 package org.xiaoxigua.fakeplayer.events
 
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
-import org.bukkit.event.player.PlayerJoinEvent
 import org.xiaoxigua.fakeplayer.FakePlayerEntity
 
 class FakePlayerInWorldEvent(private val fakePlayers: MutableList<FakePlayerEntity>) : Listener {
